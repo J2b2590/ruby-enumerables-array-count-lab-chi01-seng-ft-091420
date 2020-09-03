0838e44c9fcafce8ab5bce51dd690a9dd5945da8
Jeffrey Bolling <j2b2590@gmail.com>
2020-09-03 02:56:18 +0000

    Automatically backed up by Learn

--- lib/array_count.rb
+++ lib/array_count.rb
@@ -8,12 +8,9 @@ end
 
 def count_empty_strings(array)
   # Return the total number of EMPTY strings in the provided array using the count enumerable
-int_arr = []
-blank_arr = []
-if array.partition(&:number?)
-  int_arr << array
-elsif array.partition(&:blank?) || array.partition(&:empty?)
-  blank_arr << array
-end
-puts blank_arr
+
+array.partition(&:!present?).count
+
+
+
   end
